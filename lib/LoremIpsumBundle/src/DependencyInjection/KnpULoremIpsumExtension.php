326a9b31c5dec5caf429a0104c446c0eb60a0f46
Daniel Backes <danielbackes@protonmail.com>
2021-07-13 16:17:51 -0400

    feat: allow services to be overridden

--- lib/LoremIpsumBundle/src/DependencyInjection/KnpULoremIpsumExtension.php
+++ lib/LoremIpsumBundle/src/DependencyInjection/KnpULoremIpsumExtension.php
@@ -5,6 +5,7 @@ namespace KnpU\LoremIpsumBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class KnpULoremIpsumExtension extends Extension
@@ -19,8 +20,11 @@ class KnpULoremIpsumExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $definition = $container->getDefinition('knpu_lorem_ipsum.knpu_ipsum');
-        $definition->setArgument(0, $config['unicorns_are_real']);
-        $definition->setArgument(1, $config['min_sunshine']);
+        if (null !== $config['word_provider']) {
+            $container->setAlias('knpu_lorem_ipsum.word_provider', $config['word_provider']);
+        }
+        $definition->setArgument(1, $config['unicorns_are_real']);
+        $definition->setArgument(2, $config['min_sunshine']);
     }
 
     public function getAlias()
